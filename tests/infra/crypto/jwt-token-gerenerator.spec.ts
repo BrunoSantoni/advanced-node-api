4f0a5c3d39345f80080b252584f2060b6534015e
Bruno Santoni <brunosantoni98@gmail.com>
2022-04-15 20:00:02 -0300

    isolate SUT creation

--- tests/infra/crypto/jwt-token-gerenerator.spec.ts
+++ tests/infra/crypto/jwt-token-gerenerator.spec.ts
@@ -18,16 +18,28 @@ class JwtTokenGenerator {
 }
 
 describe('JwtTokenGenerator', () => {
-  it('should call sign with correct params', async () => {
-    const fakeJwt = jwt as jest.Mocked<typeof jwt>
-    const sut = new JwtTokenGenerator('any_secret')
+  let sut: JwtTokenGenerator
+  let fakeJwt: jest.Mocked<typeof jwt>
+  let secret: string
 
+  beforeAll(() => {
+    fakeJwt = jwt as jest.Mocked<typeof jwt>
+    secret = 'any_secret'
+  })
+
+  beforeEach(() => {
+    jest.clearAllMocks()
+
+    sut = new JwtTokenGenerator(secret)
+  })
+
+  it('should call sign with correct params', async () => {
     await sut.generateToken({
       key: 'any_key',
       expirationInMinutes: 1
     })
 
-    expect(fakeJwt.sign).toHaveBeenCalledWith({ key: 'any_key' }, 'any_secret', { expiresIn: 60 })
+    expect(fakeJwt.sign).toHaveBeenCalledWith({ key: 'any_key' }, secret, { expiresIn: 60 })
     expect(fakeJwt.sign).toHaveBeenCalledTimes(1)
   })
 })
