b3455d755cea0c9ebdcd6be34aaae32e5c1ce2f6
Bruno Santoni <brunosantoni98@gmail.com>
2022-04-27 20:03:19 -0300

    isolate facebook-api SUT creation

--- tests/external/facebook-api.test.ts
+++ tests/external/facebook-api.test.ts
@@ -3,10 +3,15 @@ import { AxiosHttpClient } from '@/infra/http'
 import { env } from '@/main/config/env'
 
 describe('Facebook API Integration Tests', () => {
-  it('should return a Facebook User if token is valid', async () => {
-    const axiosClient = new AxiosHttpClient()
-    const sut = new FacebookApi(env.facebookApi.clientId, env.facebookApi.clientSecret, axiosClient)
+  let sut: FacebookApi
+  let axiosClient: AxiosHttpClient
 
+  beforeEach(() => {
+    axiosClient = new AxiosHttpClient()
+    sut = new FacebookApi(env.facebookApi.clientId, env.facebookApi.clientSecret, axiosClient)
+  })
+
+  it('should return a Facebook User if token is valid', async () => {
     /* Token gerado pelo facebook developers expira depois de 3 meses: 27/07/22 */
     const fbUser = await sut.loadUser({ token: 'EAAXHrbQOgwoBAFJdJkgmDtSQcsZAEZB7gFcbMnWJ7S170Aj3pCxwLJEWOWlr7shnCAoiudAqUYJA2DojVsxYBSBuEGy39WlRzGsKx3yGb5TosZBUOTF8oapECGGfUuZCk9LMjc3y78wAkBhQiptfaZAtuMcI8CFX9aGjcI19JZCpbrLBM8xxUvqAlgO6ECW9rp7gUla3Lw6avxRqaVXIdR' })
 
@@ -18,9 +23,6 @@ describe('Facebook API Integration Tests', () => {
   })
 
   it('should return undefined if token is invalid', async () => {
-    const axiosClient = new AxiosHttpClient()
-    const sut = new FacebookApi(env.facebookApi.clientId, env.facebookApi.clientSecret, axiosClient)
-
     /* Token gerado pelo facebook developers expira depois de 3 meses: 27/07/22 */
     const fbUser = await sut.loadUser({ token: 'invalid' })
 
