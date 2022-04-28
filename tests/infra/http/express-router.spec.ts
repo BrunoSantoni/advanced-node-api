b3d98fa74b726db1a46cb947d0a57dc1ffa7fc87
Bruno Santoni <brunosantoni98@gmail.com>
2022-04-28 20:26:28 -0300

    ensure ExpressRouter returns 200 and valid data

--- tests/infra/http/express-router.spec.ts
+++ tests/infra/http/express-router.spec.ts
@@ -5,7 +5,9 @@ import { BaseController } from '@/application/controllers'
 class ControllerSpy extends BaseController {
   result = {
     statusCode: 200,
-    data: 'any_data'
+    data: {
+      any: 'response'
+    }
   }
 
   async perform (httpRequest: any): Promise<any> {
@@ -19,7 +21,9 @@ class ExpressRouter {
   ) {}
 
   async adapt (req: Request, res: Response): Promise<void> {
-    await this.controller.handle({ ...req.body })
+    const httpResponse = await this.controller.handle({ ...req.body })
+
+    res.status(200).json(httpResponse.data)
   }
 }
 
@@ -52,6 +56,7 @@ describe('ExpressRouter', () => {
     expect(handleSpy).toHaveBeenCalledWith({
       any: 'value'
     })
+    expect(handleSpy).toHaveBeenCalledTimes(1)
   })
 
   it('should call handle with empty request', async () => {
@@ -62,5 +67,18 @@ describe('ExpressRouter', () => {
 
     // Por enquanto só se preocupa com o body
     expect(handleSpy).toHaveBeenCalledWith({})
+    expect(handleSpy).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return with 200 and valid data', async () => {
+    await sut.adapt(req, res)
+
+    // Por enquanto só se preocupa com o body
+    expect(res.status).toHaveBeenCalledWith(200)
+    expect(res.status).toHaveBeenCalledTimes(1)
+    expect(res.json).toHaveBeenCalledWith({
+      any: 'response'
+    })
+    expect(res.json).toHaveBeenCalledTimes(1)
   })
 })
