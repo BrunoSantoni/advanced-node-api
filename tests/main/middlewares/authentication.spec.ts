15fd80f0cac3c46b10049ded65916fdfe109d959
Bruno Santoni <brunosantoni98@gmail.com>
2022-05-03 22:26:05 -0300

    ensure AuthMiddleware returns 200 if header is valid and 403 if is invalid

--- tests/main/middlewares/authentication.spec.ts
+++ tests/main/middlewares/authentication.spec.ts
@@ -1,7 +1,9 @@
 import request from 'supertest'
+import { sign } from 'jsonwebtoken'
 import { app } from '@/main/config/app'
 import { ForbiddenError } from '@/application/errors'
 import { auth } from '@/main/middlewares'
+import { env } from '@/main/config/env'
 
 describe('Authentication Middleware', () => {
   it('should return 403 if authorization header is not provided', async () => {
@@ -10,6 +12,35 @@ describe('Authentication Middleware', () => {
     })
 
     const { status, body } = await request(app).get('/fake_route')
+
+    expect(status).toBe(403)
+    expect(body.error).toBe(new ForbiddenError().message)
+  })
+
+  it('should return 200 if authorization header is valid', async () => {
+    // No teste de integração poderíamos mockar o JWT ou usar o real
+    // Foi optado por usar a lib real e gerar um token
+    const authorization = sign({ key: 'any_user_id' }, env.jwtSecret)
+    app.get('/fake_route', auth, (req, res) => {
+      res.json(req.locals)
+    })
+    const { status, body } = await request(app)
+      .get('/fake_route')
+      .set({ authorization })
+
+    expect(status).toBe(200)
+    expect(body).toEqual({
+      userId: 'any_user_id'
+    })
+  })
+
+  it('should return 403 if authorization header is invalid', async () => {
+    app.get('/fake_route', auth, (req, res) => {
+      res.json(req.locals)
+    })
+    const { status, body } = await request(app)
+      .get('/fake_route')
+      .set({ authorization: 'invalid_auth' })
 
     expect(status).toBe(403)
     expect(body.error).toBe(new ForbiddenError().message)
