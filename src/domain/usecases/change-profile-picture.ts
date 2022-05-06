02f5cdff48fb1ca50c82b7f4871be432ab5d87b5
Bruno Santoni <brunosantoni98@gmail.com>
2022-05-05 22:30:37 -0300

    ensure ChangeProfilePicture dont call FileStorage when file is undefined

--- src/domain/usecases/change-profile-picture.ts
+++ src/domain/usecases/change-profile-picture.ts
@@ -1,10 +1,14 @@
 import { UploadFile, IDGenerator } from '@/domain/contracts/gateways'
 
-type Input = { userId: string, file: Buffer }
+type Input = { userId: string, file?: Buffer }
 export type ChangeProfilePicture = (input: Input) => Promise<void>
 type Setup = (fileStorage: UploadFile, idGenerator: IDGenerator) => ChangeProfilePicture
 
 export const setupChangeProfilePicture: Setup = (fileStorage, idGenerator) => async ({ userId, file }) => {
+  if (file === undefined) {
+    return
+  }
+
   const uniqueId = idGenerator.uuid({ key: userId })
   await fileStorage.upload({ file, key: uniqueId })
 }
--- tests/domain/usecases/change-profile-picture.spec.ts
+++ tests/domain/usecases/change-profile-picture.spec.ts
@@ -36,4 +36,13 @@ describe('ChangeProfilePicture', () => {
     })
     expect(fileStorage.upload).toHaveBeenCalledTimes(1)
   })
+
+  it('should not call UploadFile when file is undefined', async () => {
+    await sut({
+      userId: 'any_id',
+      file: undefined
+    })
+
+    expect(fileStorage.upload).not.toHaveBeenCalled()
+  })
 })
