91b2822d803a29ef3934c5e23652c3779bdfb82b
Bruno Santoni <brunosantoni98@gmail.com>
2022-05-05 22:15:46 -0300

    ensure ChangeProfilePicture calls UploadFile with unique id

--- tests/domain/usecases/change-profile-picture.spec.ts
+++ tests/domain/usecases/change-profile-picture.spec.ts
@@ -1,9 +1,10 @@
 type Input = { userId: string, file: Buffer }
 type ChangeProfilePicture = (input: Input) => Promise<void>
-type Setup = (fileStorage: UploadFile) => ChangeProfilePicture
+type Setup = (fileStorage: UploadFile, idGenerator: IDGenerator) => ChangeProfilePicture
 
-const setupChangeProfilePicture: Setup = (fileStorage) => async ({ userId, file }) => {
-  await fileStorage.upload({ file, key: userId })
+const setupChangeProfilePicture: Setup = (fileStorage, idGenerator) => async ({ userId, file }) => {
+  const uniqueId = idGenerator.uuid({ key: userId })
+  await fileStorage.upload({ file, key: uniqueId })
 }
 
 export namespace UploadFile {
@@ -13,13 +14,25 @@ interface UploadFile {
   upload: (input: UploadFile.Input) => Promise<void>
 }
 
+export namespace IDGenerator {
+  export type Input = { key: string }
+  export type Output = string
+}
+interface IDGenerator {
+  uuid: (input: IDGenerator.Input) => IDGenerator.Output
+}
+
 describe('ChangeProfilePicture', () => {
   it('should call UploadFile with correct input', async () => {
+    const uuid = 'any_unique_id'
     const file = Buffer.from('any_buffer')
     const fileStorage: UploadFile = {
       upload: jest.fn(async () => await Promise.resolve())
     }
-    const sut = setupChangeProfilePicture(fileStorage)
+    const idGenerator: IDGenerator = {
+      uuid: jest.fn(() => 'any_unique_id')
+    }
+    const sut = setupChangeProfilePicture(fileStorage, idGenerator)
 
     await sut({
       userId: 'any_id',
@@ -28,7 +41,7 @@ describe('ChangeProfilePicture', () => {
 
     expect(fileStorage.upload).toHaveBeenCalledWith({
       file,
-      key: 'any_id'
+      key: uuid
     })
     expect(fileStorage.upload).toHaveBeenCalledTimes(1)
   })
