37c97406b71deecef319a45996ab8ac03d5e6f5b
Bruno Santoni <brunosantoni98@gmail.com>
2022-05-10 20:52:48 -0300

    ensure SaveProfilePictureController returns 400 if file is not provided

---
+++ tests/application/controllers/save-profile-picture.spec.ts
@@ -0,0 +1,43 @@
+import { RequiredFieldError } from '@/application/errors'
+import { badRequest, HttpResponse } from '@/application/helpers'
+
+type HttpRequest = {
+  file: any
+}
+type Model = Error
+
+class SaveProfilePictureController {
+  async handle ({ file }: HttpRequest): Promise<HttpResponse<Model>> {
+    return badRequest(new RequiredFieldError('file'))
+  }
+}
+
+describe('SaveProfilePictureController', () => {
+  let sut: SaveProfilePictureController
+
+  beforeEach(() => {
+    sut = new SaveProfilePictureController()
+  })
+
+  it('should return 400 if file is undefined', async () => {
+    const httpResponse = await sut.handle({
+      file: undefined
+    })
+
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: new RequiredFieldError('file')
+    })
+  })
+
+  it('should return 400 if file is null', async () => {
+    const httpResponse = await sut.handle({
+      file: null
+    })
+
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: new RequiredFieldError('file')
+    })
+  })
+})
