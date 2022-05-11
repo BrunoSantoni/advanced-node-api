85ef975b73d7ee730d0ac088184cafa66c4d10ee
Bruno Santoni <brunosantoni98@gmail.com>
2022-05-10 21:14:12 -0300

    ensure SaveProfilePictureController returns 400 if mimetype is invalid

--- tests/application/controllers/save-profile-picture.spec.ts
+++ tests/application/controllers/save-profile-picture.spec.ts
@@ -2,18 +2,40 @@ import { RequiredFieldError } from '@/application/errors'
 import { badRequest, HttpResponse } from '@/application/helpers'
 
 type HttpRequest = {
-  file: any
+  file: { buffer: Buffer, mimeType: string }
 }
 type Model = Error
 
+class InvalidMymeTypeError extends Error {
+  constructor (allowed: string[]) {
+    super(`Unsupported type. Allowed types: ${allowed.join(', ')}`)
+    this.name = 'InvalidMymeTypeError'
+  }
+}
+
 class SaveProfilePictureController {
   async handle ({ file }: HttpRequest): Promise<HttpResponse<Model>> {
-    return badRequest(new RequiredFieldError('file'))
+    if (file === undefined || file === null) {
+      return badRequest(new RequiredFieldError('file'))
+    }
+    if (file.buffer.length === 0) {
+      return badRequest(new RequiredFieldError('file'))
+    }
+
+    return badRequest(new InvalidMymeTypeError(['png', 'jpg']))
   }
 }
 
 describe('SaveProfilePictureController', () => {
   let sut: SaveProfilePictureController
+
+  let buffer: Buffer
+  let mimeType: string
+
+  beforeAll(() => {
+    buffer = Buffer.from('any_buffer')
+    mimeType = 'image/png'
+  })
 
   beforeEach(() => {
     sut = new SaveProfilePictureController()
@@ -21,7 +43,7 @@ describe('SaveProfilePictureController', () => {
 
   it('should return 400 if file is undefined', async () => {
     const httpResponse = await sut.handle({
-      file: undefined
+      file: undefined as any
     })
 
     expect(httpResponse).toEqual({
@@ -32,7 +54,7 @@ describe('SaveProfilePictureController', () => {
 
   it('should return 400 if file is null', async () => {
     const httpResponse = await sut.handle({
-      file: null
+      file: null as any
     })
 
     expect(httpResponse).toEqual({
@@ -44,13 +66,31 @@ describe('SaveProfilePictureController', () => {
   it('should return 400 if file is empty', async () => {
     const httpResponse = await sut.handle({
       file: {
-        buffer: Buffer.from('')
+        buffer: Buffer.from(''),
+        mimeType
       }
     })
 
     expect(httpResponse).toEqual({
       statusCode: 400,
       data: new RequiredFieldError('file')
+    })
+  })
+
+  it('should return 400 if file type is invalid', async () => {
+    const httpResponse = await sut.handle({
+      file: {
+        buffer,
+        mimeType: 'invalid_mimetype'
+      }
+    })
+
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: new InvalidMymeTypeError([
+        'png',
+        'jpg'
+      ])
     })
   })
 })
