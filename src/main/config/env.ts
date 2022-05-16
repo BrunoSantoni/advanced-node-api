9c331f98b46fbd3bdf5034b9c9043b95f01c6ace
Bruno Santoni <brunosantoni98@gmail.com>
2022-05-16 19:45:45 -0300

    add aws s3 integration test

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -4,5 +4,10 @@ export const env = {
     clientSecret: process.env.FB_CLIENT_SECRET ?? 'c1490c22aad5b8fb4a163de94d9158aa'
   },
   jwtSecret: process.env.JWT_SECRET ?? 'f68sd4f684',
-  port: process.env.PORT ?? 3333
+  port: process.env.PORT ?? 3333,
+  s3: {
+    accessKey: process.env.AWS_ACCESS_KEY ?? '',
+    secret: process.env.AWS_SECRET_KEY ?? '',
+    bucket: process.env.AWS_S3_BUCKET ?? ''
+  }
 }
---
+++ tests/external/s3-file-storage.test.ts
@@ -0,0 +1,40 @@
+import axios from 'axios'
+import { S3FileStorage } from '@/infra/sdks'
+import { env } from '@/main/config/env'
+
+describe('AWS S3 Integration Tests', () => {
+  let key: string
+  let file: Buffer
+  let onePixelImage: string
+
+  let sut: S3FileStorage
+
+  beforeAll(() => {
+    key = 'any_key.png'
+    onePixelImage = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAA1JREFUGFdjME/c8x8ABHsCVDusIskAAAAASUVORK5CYII='
+    file = Buffer.from(onePixelImage, 'base64')
+  })
+
+  beforeEach(() => {
+    sut = new S3FileStorage(
+      env.s3.accessKey,
+      env.s3.secret,
+      env.s3.bucket
+    )
+  })
+
+  it('should upload and delete image from aws s3', async () => {
+    const pictureUrl = await sut.upload({
+      key,
+      file
+    })
+
+    // Testa com o Axios se a URL obtida da imagem é válida.
+    expect((await axios.get(pictureUrl)).status).toBe(200)
+
+    await sut.delete({ key })
+
+    // Testa com o Axios se a URL obtida da imagem foi deletada.
+    await expect(axios.get(pictureUrl)).rejects.toThrow()
+  })
+})
